02be58c95b220a42397f76de203df593a4a44d9f
mi2ntae <kimmintae981019@gmail.com>
2021-11-21 12:56:20 +0900

    Initialize

--- Insurance_2021/src/main/java/com/insurance/sce/controller/HomeController.java
+++ Insurance_2021/src/main/java/com/insurance/sce/controller/HomeController.java
@@ -4,12 +4,15 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -24,7 +27,7 @@ public class HomeController {
 	 */
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model, HttpServletRequest request) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -42,5 +45,10 @@ public class HomeController {
 		return "response";
 	}
 
-	
+	@RequestMapping(value = "/example.do")
+	public ModelAndView example(HttpServletRequest request) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("example");
+		return mv;
+	}
 }

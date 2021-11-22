da3a6f973d8c1616d44ac3d0ea4ce4ad9ef35416
YoungSeokHong <80624082+YoungSeokHong@users.noreply.github.com>
2021-11-23 02:41:46 +0900

    고객 보험리스트 보기 화면 수정, css 파일 정렬

--- Insurance_2021/src/main/java/com/insurance/sce/controller/customer/SelectCacnerInsuranceController.java
+++ Insurance_2021/src/main/java/com/insurance/sce/controller/customer/SelectCacnerInsuranceController.java
@@ -3,15 +3,19 @@ package com.insurance.sce.controller.customer;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.insurance.sce.dao.AccidentDAO;
+import com.insurance.sce.dao.AccidentDAOImpl;
 import com.insurance.sce.model.customer.Customer;
 
 /**
@@ -23,16 +27,24 @@ public class SelectCacnerInsuranceController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SelectCacnerInsuranceController.class);
 	
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	
 	@RequestMapping(value="cancerInsurance", method=RequestMethod.GET)
 	public String response(Locale locale, Model model, HttpServletRequest request) {
-		Customer customer = new Customer();
-		customer.setName("홍영석");
+		HttpSession session = request.getSession(true);
+		
+		//임시방편
+		Customer tmp = new Customer();
+		tmp.setName("홍영석");
+		session.setAttribute("loginCustomer", tmp);
+		
+		Customer customer = (Customer)session.getAttribute("loginCustomer");
+		
 
-		model.addAttribute("clientName", customer.getName() );
+		model.addAttribute("customerName", customer.getName() );
 		return "customer/selectCancerInsurance";
 	}
 }
